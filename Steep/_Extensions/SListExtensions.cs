9575acf03470ba6b5f0f92433f92948d33d555f0
arekbal <arek_bal@wp.pl>
2019-08-11 00:27:27 +0200

    Emplace Back if change

--- Steep/_Extensions/SListExtensions.cs
+++ Steep/_Extensions/SListExtensions.cs
@@ -2,11 +2,12 @@ namespace Steep {
   public static class SListExtensions {
     public static ref T EmplaceBack<T>(this SList<T> that) where T: struct
     {
-      if(that._items.Length > that._size)
-        that._size++;      
-      else
-        that.ReserveItems(1);
-
+      if(that._items.Length > that._size) {
+        that._size++; // shorter path with less calls...        
+        return ref that._items[that._size - 1];
+      }
+     
+      that.ReserveItems(1);
       return ref that._items[that._size - 1];
     }
   }
